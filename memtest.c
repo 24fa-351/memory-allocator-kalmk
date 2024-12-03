37ccf918d656c9c3e665f9eab32df78029af8133
kalmk <111930167+kalmk@users.noreply.github.com>
2024-12-03 08:44:37 -0800

    in progress

--- memtest.c
+++ memtest.c
@@ -5,69 +5,84 @@
 
 // define "x" for system malloc, include for our versions. Don't do both.
 #ifdef SYSTEM_MALLOC
-#define xfree free
-#define xmalloc malloc
+#define free free
+#define malloc malloc
 #else
 #include "malloc.h"
 #endif
 
-int rand_between(int min, int max) { return rand() % (max - min + 1) + min; }
+int rand_between(int min, int max)
+{
+    return rand() % (max - min + 1) + min;
+}
 
 #define TEST_SIZE 30
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-int main(int argc, char *argv[]) {
-  srand(time(NULL));
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
 
-  char *test_string = "Now is the time for all good people to come to the aid "
-                      "of their country.";
+    char *test_string =
+        "Now is the time for all good people to come to the aid "
+        "of their country.";
 
-  if (argc > 1) {
-    test_string = argv[1];
-  }
-
-  char *ptrs[TEST_SIZE];
-
-  for (int ix = 0; ix < TEST_SIZE; ix++) {
-    int size = rand_between(1, strlen(test_string)+1);
-    fprintf(stderr, "\n\n\n[%d] size: %d\n", ix, size);
-
-    ptrs[ix] = xmalloc(size);
-    if (ptrs[ix] == NULL) {
-      printf("[%d] malloc failed\n", ix);
-      exit(1);
+    if (argc > 1)
+    {
+        test_string = argv[1];
     }
 
-    int len_to_copy = MIN(strlen(test_string), size - 1);
+    char *ptrs[TEST_SIZE];
 
-    // fprintf(stderr, "[%d] ptrs[%d]: %p, going to copy %d chars\n", ix, ix,
-    //         ptrs[ix], len_to_copy);
+    for (int ix = 0; ix < TEST_SIZE; ix++)
+    {
+        int size = rand_between(1, strlen(test_string) + 1);
+        fprintf(stderr, "\n\n\n[%d] size: %d\n", ix, size);
 
-    strncpy(ptrs[ix], test_string, len_to_copy);
-    ptrs[ix][len_to_copy] = '\0';
+        ptrs[ix] = malloc(size);
+        if (ptrs[ix] == NULL)
+        {
+            printf("[%d] malloc failed\n", ix);
+            exit(1);
+        }
 
-    fprintf(stderr, "[%x] '%s'\n", ix, ptrs[ix]);
+        int len_to_copy = MIN(strlen(test_string), size - 1);
 
-    int index_to_free = rand_between(0, ix);
-    if (ptrs[index_to_free]) {
-      fprintf(stderr, "\n[%d] randomly freeing %p ('%s')\n", index_to_free,
-              ptrs[index_to_free], ptrs[index_to_free]);
-      xfree(ptrs[index_to_free]);
-      fprintf(stderr, "[%d] freed %p\n", index_to_free, ptrs[index_to_free]);
-      ptrs[index_to_free] = NULL;
+        // fprintf(stderr, "[%d] ptrs[%d]: %p, going to copy %d chars\n", ix,
+        // ix,
+        //         ptrs[ix], len_to_copy);
+
+        strncpy(ptrs[ix], test_string, len_to_copy);
+        ptrs[ix][len_to_copy] = '\0';
+
+        fprintf(stderr, "[%x] '%s'\n", ix, ptrs[ix]);
+
+        int index_to_free = rand_between(0, ix);
+        if (ptrs[index_to_free])
+        {
+            fprintf(stderr, "\n[%d] randomly freeing %p ('%s')\n",
+                    index_to_free, ptrs[index_to_free], ptrs[index_to_free]);
+            free(ptrs[index_to_free]);
+            fprintf(stderr, "[%d] freed %p\n", index_to_free,
+                    ptrs[index_to_free]);
+            ptrs[index_to_free] = NULL;
+        }
     }
-  }
 
-  for (int ix = 0; ix < TEST_SIZE; ix++) {
-    if (ptrs[ix]) {
-      fprintf(stderr, "[%d] freeing %p (%s)\n", ix, ptrs[ix], ptrs[ix]);
-      xfree(ptrs[ix]);
-      fprintf(stderr, "[%d] freed %p\n", ix, ptrs[ix]);
-    } else {
-      fprintf(stderr, "[%d] already freed\n", ix);
+    for (int ix = 0; ix < TEST_SIZE; ix++)
+    {
+        if (ptrs[ix])
+        {
+            fprintf(stderr, "[%d] freeing %p (%s)\n", ix, ptrs[ix], ptrs[ix]);
+            free(ptrs[ix]);
+            fprintf(stderr, "[%d] freed %p\n", ix, ptrs[ix]);
+        }
+        else
+        {
+            fprintf(stderr, "[%d] already freed\n", ix);
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
--- test.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_H
-#define TEST_H
-
-void test_malloc();
-void test_free();
-void test_realloc();
-void run_tests();
-
-#endif // TEST_H
